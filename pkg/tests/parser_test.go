5e7e398c73b649e129cdb76f7c2d98a34a95915c
lllchak <mediumchak@yandex.ru>
2023-10-03 21:10:31 +0300

    lllchak - added numeric types tests

--- pkg/tests/parser_test.go
+++ pkg/tests/parser_test.go
@@ -49,6 +49,79 @@ func TestParse(t *testing.T) {
 				},
 			},
 		},
+		{
+			source: `SELECT id, name FROM "sketchy name";`,
+			ast: &ast.TSyntaxTree{
+				Statements: []*ast.TStatement{
+					{
+						Type: ast.SelectType,
+						Select: &ast.TSelectStatement{
+							Rules: []*ast.TExpression{
+								{
+									Type: ast.LiteralType,
+									Literal: &lexer.TToken{
+										Loc:   lexer.TTokenLocation{Column: 7, Line: 0},
+										Type:  lexer.IdentifierType,
+										Value: "id",
+									},
+								},
+								{
+									Type: ast.LiteralType,
+									Literal: &lexer.TToken{
+										Loc:   lexer.TTokenLocation{Column: 11, Line: 0},
+										Type:  lexer.IdentifierType,
+										Value: "name",
+									},
+								},
+							},
+							From: lexer.TToken{
+								Loc:   lexer.TTokenLocation{Column: 21, Line: 0},
+								Type:  lexer.IdentifierType,
+								Value: "sketchy name",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			source: `SELECT 1, 2, 3;`,
+			ast: &ast.TSyntaxTree{
+				Statements: []*ast.TStatement{
+					{
+						Type: ast.SelectType,
+						Select: &ast.TSelectStatement{
+							Rules: []*ast.TExpression{
+								{
+									Type: ast.LiteralType,
+									Literal: &lexer.TToken{
+										Loc:   lexer.TTokenLocation{Column: 7, Line: 0},
+										Type:  lexer.NumericType,
+										Value: "1",
+									},
+								},
+								{
+									Type: ast.LiteralType,
+									Literal: &lexer.TToken{
+										Loc:   lexer.TTokenLocation{Column: 11, Line: 0},
+										Type:  lexer.NumericType,
+										Value: "2",
+									},
+								},
+								{
+									Type: ast.LiteralType,
+									Literal: &lexer.TToken{
+										Loc:   lexer.TTokenLocation{Column: 15, Line: 0},
+										Type:  lexer.NumericType,
+										Value: "3",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 
 	for _, test := range tests {
